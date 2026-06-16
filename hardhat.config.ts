16d9be2c4d480a707302535f4c41b7a0db458c70
humbertoromanojr <astronomi@gmail.com>
2026-06-16 09:06:27 -0300

    Initial setup complete

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable, defineConfig } from "hardhat/config";
 
@@ -20,19 +21,25 @@ export default defineConfig({
     },
   },
   networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
+    local: {
+      type: "http",
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
     },
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      chainId: Number(process.env.CHAIN_ID),
+      url: configVariable("RPC_NODE"),
+      accounts: [configVariable("PRIVATE_KEY")],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: process.env.API_KEY,
     },
   },
 });
