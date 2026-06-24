69cc4c3bc2398f7f9ea3efd6ff90fa66313e8964
humbertoromanojr <astronomi@gmail.com>
2026-06-24 14:32:44 -0300

    Implementing Should transferFrom, Should NOT transferFrom (balance) and Should NOT transferFrom (allowance)

--- test/ProtoCoin.test.ts
+++ test/ProtoCoin.test.ts
@@ -79,4 +79,51 @@ describe("ProtoCoin", function () {
     const value = await contract.allowance(owner.address, otherAccount.address);
     expect(value).to.equal(1n);
   });
+
+  it("Should transferFrom", async function () {
+    const contract = await ethers.deployContract("ProtoCoin");
+    const balanceOwnerBefore = await contract.balanceOf(owner.address);
+    const balanceOtherBefore = await contract.balanceOf(otherAccount.address);
+
+    await contract.approve(otherAccount.address, 10n);
+
+    const instance = contract.connect(otherAccount);
+    await instance.transferFrom(owner.address, otherAccount.address, 5n);
+
+    const balanceOwnerAfter = await contract.balanceOf(owner.address);
+    const balanceOtherAfter = await contract.balanceOf(otherAccount.address);
+
+    const allowance = await contract.allowance(
+      owner.address,
+      otherAccount.address,
+    );
+
+    expect(balanceOwnerBefore).to.equal(1000n * 10n ** 18n);
+    expect(balanceOwnerAfter).to.equal(1000n * 10n ** 18n - 5n);
+
+    expect(balanceOtherBefore).to.equal(0n);
+    expect(balanceOtherAfter).to.equal(5n);
+
+    expect(allowance).to.equal(5n);
+  });
+
+  it("Should NOT transferFrom (balance)", async function () {
+    const contract = await ethers.deployContract("ProtoCoin");
+
+    const instance = contract.connect(otherAccount);
+
+    await expect(
+      instance.transferFrom(otherAccount.address, otherAccount, 1n),
+    ).to.revertedWith("Insufficient balance");
+  });
+
+  it("Should NOT transferFrom (allowance)", async function () {
+    const contract = await ethers.deployContract("ProtoCoin");
+
+    const instance = contract.connect(otherAccount);
+
+    await expect(
+      instance.transferFrom(owner.address, otherAccount, 1n),
+    ).to.revertedWith("Insufficient allowance");
+  });
 });
