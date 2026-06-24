1fbe40ca1f0cb00ec857f73c98eb4bc5612cba9a
humbertoromanojr <astronomi@gmail.com>
2026-06-24 10:19:44 -0300

    Implement balanceOf, transfer, approve and allowance

--- contracts/ProtoCoin.sol
+++ contracts/ProtoCoin.sol
@@ -6,4 +6,50 @@ contract ProtoCoin {
     string public symbol = "PRC";
     uint8 public decimals = 18;
     uint256 public totalSupply = 1000 * 10 ** decimals; 
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner, 
+        address indexed _spender, 
+        uint256 _value
+    );
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(){
+        // owner get all coin
+        _balances[msg.sender] = totalSupply;
+    }
+
+    // Check Someone's Balance - address _owner
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        // verifica saldo
+        return _balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // verifica se tem fundos para a transferência
+        require(balanceOf(msg.sender) >= _value, "Insufficient balance");
+
+        _balances[msg.sender] -= _value;
+        _balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        _allowances[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // Transitions permissions
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return _allowances[_owner][_spender];
+    }
 }
